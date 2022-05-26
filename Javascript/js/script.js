a35f3f86bb046ac6d0ead242341f4581f5d40739
rathodkrishna886 <104242404+rathodkrishna886@users.noreply.github.com>
2022-05-26 19:02:00 +0530

    message

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -1,6 +1,7 @@
-function $(id)
-{
-    return document.getElementById(id);
-}
+//function $(id)
+//{
+ //   return document.getElementById(id);
+//}
+let $ = id => document.getElementById(id);
 let int = (number) => parseInt(number);
-var number = document.getElementById("number").value;
+// var number = document.getElementById("number").value;
