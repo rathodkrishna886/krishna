29a7e4f8197c714f228eb31119094662f8c4c264
rathodkrishna886 <104242404+rathodkrishna886@users.noreply.github.com>
2022-05-30 20:00:18 +0530

    message

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -4,5 +4,10 @@
 //}
 let $ = id => document.getElementById(id);
 let _ = name => document.getElementByname(name);
-//let int = (number) => parseInt(number);
- var number = document.getElementByname("number").value;
+let int = (number) => parseInt(number);
+ var number = document.getElementByid("number").value;
+let getLength = (number) => String(number).length
+let int = (number) => parseInt(number);
+let float = (number) => parseFloat(number);
+let log = (message) => console.log(message)
+let error = (error_message) => console.error(error_message)
