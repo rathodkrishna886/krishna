8cd20e3587246945746e69da13e4f3500e4466d5
rathodkrishna886 <104242404+rathodkrishna886@users.noreply.github.com>
2022-06-08 20:58:14 +0530

    Message

--- Javascript/js/script.js
+++ Javascript/js/script.js
@@ -1,13 +1,13 @@
-//function $(id)
-//{
- //   return document.getElementById(id);
-//}
-let $ = id => document.getElementById(id);
-let _ = name => document.getElementByname(name);
-//let int = (number) => parseInt(number);
- var number = document.getElementById("number").value;
-let getLength = (number) => String(number).length
-let int = (number) => parseInt(number);
-let float = (number) => parseFloat(number);
-let log = (message) => console.log(message)
-let error = (error_message) => console.error(error_message)
+    //function $(id)
+    //{
+    // return document.getElementById(id);
+    //}
+    //let $ = id => document.getElementById(id);
+    let $ = name => document.getElementById(name);
+    //let int = (number) => parseInt(number);
+    var number = document.getElementById("number").value;
+    let getLength = (number) => String(number).length
+    let int = (number) => parseInt(number);
+    let float = (number) => parseFloat(number);
+    let log = (message) => console.log(message)
+    let error = (error_message) => console.error(error_message)
