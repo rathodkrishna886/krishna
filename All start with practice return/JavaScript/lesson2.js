970f08a67a58d93e8db1bbe801ceb7fe8e5201ac
rathodkrishna886 <104242404+rathodkrishna886@users.noreply.github.com>
2022-09-01 22:07:49 +0530

    Message

---
+++ All start with practice return/JavaScript/lesson2.js
@@ -0,0 +1,6 @@
+alert("javascript (js) is working");
+console.log("this is log msg")
+console.warn("this is warn msg")
+console.error("this is error msg")
+console.debug("this is debug msg")
+console.info("this is info msg")
